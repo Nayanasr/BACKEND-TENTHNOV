584ffbc138623249a3fa2b6baea545953d256740
Nayana Ramesh <107012@tfcorp.com>
2022-11-30 19:28:50 +0530

    Backend

---
+++ TenthNov/Delegates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenthNov
+{
+    public delegate void mydele(int x, int y);
+
+    public class Delegates
+    {
+
+
+
+        public void add(int x, int y) {
+                Console.WriteLine("The Sum is " + (x + y));
+            }
+
+            public void sub(int x, int y) {
+                Console.WriteLine("The Difference is " + (x - y));
+                Console.ReadKey();
+            }
+        
+    }
+}
---
+++ TenthNov/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TenthNov
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Delegates delegates = new Delegates();
+            //make an object of class A
+            mydele m = new mydele(delegates.add);
+            m(10, 20);
+            mydele m1 = new mydele(delegates.sub);
+            m1(10, 20);
+
+    }
+    }
+}
